7eb1c1b85d63c2ad16f39c0b8b14ebaed0cd35ff
PonPond <137371401+PonPond@users.noreply.github.com>
2023-09-13 14:57:47 +0700

    image

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -43,34 +43,10 @@ class CarController extends Controller
             ],
 
         );
-
-        $room_image = $request->file('img1');
-        $name_gen = hexdec(uniqid());
-        $img_ext = strtolower($room_image->getClientOriginalExtension());
-        $img_name = $name_gen . '.' . $img_ext;
-        $upload_location = 'img/car/';
-        $full_path1 = $upload_location . $img_name;
-        #2
-
-        $room_image2 = $request->file('img2');
-        $name_gen2 = hexdec(uniqid());
-        $img_ext2 = strtolower($room_image2->getClientOriginalExtension());
-        $img_name2 = $name_gen2 . '.' . $img_ext2;
-        $upload_location2 = 'img/car/';
-        $full_path2 = $upload_location2 . $img_name2;
-
-        #3
-        // $room_image3 = $request->file('img3');
-        // $name_gen3 = hexdec(uniqid());
-        // $img_ext3 = strtolower($room_image3->getClientOriginalExtension());
-        // $img_name3 = $name_gen3 . '.' . $img_ext3;
-        // $upload_location3 = 'img/car/';
-        // $full_path3 = $upload_location3 . $img_name3;
-        
         $client = new Client();
-        if ($request->hasFile('img3')) {
-            $file = $request->file('img3');
 
+        if ($request->hasFile('img1')) {
+            $file = $request->file('img1');
             $options = [
                 'multipart' => [
                     [
@@ -92,9 +68,63 @@ class CarController extends Controller
             $response = $client->send($guzzleRequest, $options);
             $responseBody = $response->getBody()->getContents();
             $responseData = json_decode($responseBody, true);
-            $urls = $responseData['data']['url'];
+            $urls1 = $responseData['data']['url'];
+        } else {
+            return response()->json(['error' => 'No files found in the request'], 400);
+        }
 
+        if ($request->hasFile('img2')) {
+            $file = $request->file('img2');
+            $options = [
+                'multipart' => [
+                    [
+                        'name' => 'status',
+                        'contents' => 'active'
+                    ],
+                    [
+                        'name' => 'file[]',
+                        'contents' => fopen($file->getRealPath(), 'r'),
+                        'filename' => $file->getClientOriginalName(),
+                        'headers' => [
+                            'Content-Type' => $file->getClientMimeType()
+                        ]
+                    ]
+                ]
+            ];
 
+            $guzzleRequest = new GuzzleRequest('POST', 'http://154.26.133.10:8808/api/v1/medias');
+            $response = $client->send($guzzleRequest, $options);
+            $responseBody = $response->getBody()->getContents();
+            $responseData = json_decode($responseBody, true);
+            $urls2 = $responseData['data']['url'];
+        } else {
+            return response()->json(['error' => 'No files found in the request'], 400);
+        }
+        
+        if ($request->hasFile('img3')) {
+            $file = $request->file('img3');
+            $options = [
+                'multipart' => [
+                    [
+                        'name' => 'status',
+                        'contents' => 'active'
+                    ],
+                    [
+                        'name' => 'file[]',
+                        'contents' => fopen($file->getRealPath(), 'r'),
+                        'filename' => $file->getClientOriginalName(),
+                        'headers' => [
+                            'Content-Type' => $file->getClientMimeType()
+                        ]
+                    ]
+                ]
+            ];
+
+            $guzzleRequest = new GuzzleRequest('POST', 'http://154.26.133.10:8808/api/v1/medias');
+            $response = $client->send($guzzleRequest, $options);
+            $responseBody = $response->getBody()->getContents();
+            $responseData = json_decode($responseBody, true);
+            $urls3 = $responseData['data']['url'];
         } else {
             return response()->json(['error' => 'No files found in the request'], 400);
         }
@@ -108,14 +138,10 @@ class CarController extends Controller
         $car->category = $request->category;
         $car->colors = $request->colors;
         $car->price =$request->price;
-        $car->img1 = $full_path1;
-        $car->img2 = $full_path2;
-        $car->img3 = $urls[0];
+        $car->img1 = $urls1[0];
+        $car->img2 = $urls2[0];
+        $car->img3 = $urls3[0];
         $car->save();
-
-
-        $room_image->move($upload_location, $img_name);
-        $room_image2->move($upload_location, $img_name2);
 
 
         return redirect()->route('index_car')->with('success', "บันทึกข้อมูลเรียบร้อย");
