5b0b3fbb947f5d985c8678d5b6405c70d6bf8d8f
PonPond <137371401+PonPond@users.noreply.github.com>
2023-10-09 13:43:34 +0700

    test

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserProfile;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 class UserProfileController extends Controller
 {
     public function index()
@@ -12,4 +15,50 @@ class UserProfileController extends Controller
         return view('page.users.index', compact('users'));
 
     }
+    public function indexapi()
+    {
+    $userProfiles = UserProfile::all();
+    return response()->json($userProfiles);
+    }
+
+    public function login(Request $request)
+    {
+        // ตรวจสอบข้อมูลที่รับมาจาก Request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json(['error' => 'Invalid data'], 400);
+        }
+    
+        // ค้นหาข้อมูลผู้ใช้จากฐานข้อมูล
+        $user = UserProfile::where('email', $request->input('email'))->first();
+    
+        if (!$user) {
+            // ไม่พบผู้ใช้
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    
+        // เปรียบเทียบรหัสผ่าน (ไม่มีการเข้ารหัส) กับรหัสผ่านในฐานข้อมูล
+        if ($request->input('password') !== $user->password) {
+            // รหัสผ่านไม่ตรงกัน
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    
+        // เข้าสู่ระบบสำเร็จ
+        $responseData = [
+            'id' => $user->id,
+            'name' => $user->name,
+            'tel' => $user->tel
+        ];
+    
+        return response()->json(['user' => $responseData], 200);
+    }
+    
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/user-profiles', [UserProfileController::class, 'indexapi']);
+Route::post('/login', [UserProfileController::class,'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
